9b6a18d8b32e15b8bfd5ef0a0a49b4dbbbd0d041
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-10-23 14:43:32 +0800

    build: initial project

---
+++ src/main/java/com/github/johnnymillergh/scacryptographyserver/ScaCryptographyServerApplication.java
@@ -0,0 +1,18 @@
+package com.github.johnnymillergh.scacryptographyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: ScaCryptographyServerApplication, change description here.
+ *
+ * @author Johnny Miller (锺俊), email: johnnysviva@outlook.com, date: 10/23/2020 11:39 AM
+ **/
+@SpringBootApplication
+public class ScaCryptographyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaCryptographyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/johnnymillergh/scacryptographyserver/ScaCryptographyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.johnnymillergh.scacryptographyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaCryptographyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
